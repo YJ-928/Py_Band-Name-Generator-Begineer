59a03b5d95f77c055bfc9ba48e2157b6c156ae02
YJ-928 <yashbjoshi.99@gmail.com>
2023-04-27 14:49:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Generates a unique BandName using user's city & pet names.
+# Generates a unique Band Name using user's city & pet names.
 print("Welcome to Band Name generator by YJ-928")
 city = input("Enter the city you grew up in:\n")
 pet = input("Enter the name of your pet:\n")
